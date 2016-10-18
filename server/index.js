638a493a4c5d9b18fa1f8f37acfda27298c29f67
yolixtly <yolixtly_13@hotmail.com>
2016-10-18 11:48:36 -0400

    Board is now populated with the Ideas content!!!

--- server/index.js
+++ server/index.js
@@ -32,19 +32,6 @@ function Idea(title, voteCount){
 //     console.log(board);
 // });
 
-/*Creates a new Idea Works!! */
-// var item = new Idea({
-//     ideaTitle: 'Awesome!',
-//     voteCount: 15
-// });
-
-// item.save(function(err, item){
-//         if(err) {
-//             console.log(err);
-//         }
-//         console.log('new idea', item);
-// });
-
 /*Finds a Board and Creates a new Idea: it Works! */
 Board.findOne({title: 'TestBoard'}, function(err, board){
     if(err){
@@ -52,12 +39,13 @@ Board.findOne({title: 'TestBoard'}, function(err, board){
     }
     console.log('Board before Ideas: ', board);
 
-    /*Creates a new Idea  Works!*/
+    /*STEP 1: Create a new Idea Works!*/
     var item = new Idea({
-        ideaTitle: 'Cool!',
+        ideaTitle: 'Last test Before endpoints!!',
         voteCount: 15
     });
 
+    /*STEP 2: Save the Idea Works!*/
     item.save(function(err, item){
             if(err) {
                 console.log(err);
@@ -66,7 +54,7 @@ Board.findOne({title: 'TestBoard'}, function(err, board){
     });
 
     /*this works by saving the reference of the Idea Works!*/
-
+    /* STEP 3: Push the Idea into the Board.ideas Array section, this will only save the reference */
     board.ideas.push(item);
     board.save(function(err, boardUpdate){
         if(err){
@@ -74,43 +62,18 @@ Board.findOne({title: 'TestBoard'}, function(err, board){
         }
         console.log('board after ideas added: ', boardUpdate);
     });
-    
-    /*The next approach doesnt work*/
 
-   // var testIdea = Idea.create({
-   //      _creator: board._id,
-   //      ideaTitle: 'AwesomeIdea',
-   //      voteCount: 5
-   //  }, function(err, idea){
-   //      if(err){
-   //          console.log('idea error ', err);
-   //      }
-   //      console.log(idea);
-   //  });
 
-   // board.ideas.push(testIdea);
-   // board.save(function(err, boardUpdate){
-   //      if(err){
-   //          console.log('update error ', err);
-   //      }
-   //      console.log('board after ideas added: ', boardUpdate);
-   //  })
 });
-
-
-
-/*Lets us see the data of both Board and Idea in the Console but Idea is outside Board.ideas*/
-
-// Board.findOne({title: 'TestBoard'}).populate('ideas').exec(function(err, board){
-//     console.log('board with ideas : ', board);
-// console.log(JSON.stringify(boards, null, 2));
-// });
-
-// Idea.find().exec(function(err, idea){
-//     console.log('find ideas: ', idea);
-// });
-
-
+    
+    /*Step 4: Populate your ideas with the actual objects, rather than just their reference! */
+    Board.findOne({title: 'TestBoard'}).populate('ideas').exec(function(err, board){
+        if(err){
+            console.log('error populate', err);
+        }
+    // console.log(JSON.stringify(boards, null, 2));
+    console.log('Updated Board after populate!!', board);
+    });
 
 
 /*Connection to MongoDB/mongoose */
