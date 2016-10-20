cf55ae6ef06c8451859d4c75a7f82e441ac4e7e0
yolixtly <yolixtly_13@hotmail.com>
2016-10-20 18:23:17 -0400

    fixed presets for babel on webpack.config.js

--- client/js/index.js
+++ client/js/index.js
@@ -1,20 +1,30 @@
-import 'babel-polyfill';
+// require("babel-polyfill");
 var React = require('react');
 var ReactDOM = require('react-dom');
+//React-Redux 
+// var connect = require('react-redux').connect;
+// var store = require('./client/js/store');
+// var Provider = require('react-redux').Provider;
+// var actions = require('./client/js/actions');
+
+//React-Route
+// var router = require('react-router');
+// var Router = router.Router;
+// var Route = router.Route;
+// var IndexRoute = router.IndexRoute;
+// var hashHistory = router.hashHistory;
+// var Link = router.Link;
 
 //components
-// var LandingPage = require('./components/landing-page.js');
+// var LandingPage = require('./components/l
+// anding-page.js');
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
 
 
-// var HelloWorld = function() {
-//     return (
-//         <div>Hello World</div>
-//     );
-// };
+var HelloWorld = React.createClass({render: function(){return(<h1>hi</h1>)}});
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     ReactDOM.render(<HelloWorld />, document.getElementById('app'));
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+});
 
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ module.exports = {
           test: /\.js$/,
           exclude: /(node_modules)/,
           loader: 'babel',
+          query: {presets: ['es2015', 'react']}
         },
       ]
     },
