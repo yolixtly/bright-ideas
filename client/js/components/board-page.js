e0a57a908aea9664a94c02845b00f3c9966eddb2
yolixtly <yolixtly_13@hotmail.com>
2016-10-21 14:55:55 -0400

    Ideas are render in order and are updated in order as well

--- client/js/components/board-page.js
+++ client/js/components/board-page.js
@@ -9,20 +9,31 @@ var BoardPage = React.createClass({
 		var boardTitle = this.props.BoardTitle;
 		this.props.dispatch(actions.updateVoteCount(ideaTitle, boardTitle));
 		this.props.dispatch(actions.fetchABoard(boardTitle));
-		
+
 	},
-	render: function(){
+	sortedIdeas(){
 		var self = this;
-		var ideaList = this.props.BoardIdeas.map(function(idea, index){
+		var unSortedList = this.props.BoardIdeas;
+
+		console.log('unSortedList', unSortedList);
+		var sortedList = unSortedList.sort((ideaA, ideaB) => {
+      		return ideaB.voteCount - ideaA.voteCount;
+    	});
+    	console.log("sortedList", sortedList);
+
+    	var ideaList = this.props.BoardIdeas.map(function(idea, index){
 			return (
 				<Idea key={index} votes={idea.voteCount} ideaTitle={idea.ideaTitle} handleUpVote={self.handleUpVote}/>
 			)
 		});
+		return ideaList;
+	},
+	render: function(){
 		return (
 			<div className="BoardPage wrapper">
 				<h1>{this.props.BoardTitle}</h1>
 				<hr />
-				{ideaList}
+				{this.sortedIdeas()}
 			</div>
 		);
 	}
